d4d79026948bab741d9a0f0934a7378701aee4c6
Fernando José Silva Lima Filho <93550626+FernandoLimaFilho@users.noreply.github.com>
2022-05-13 21:21:45 -0300

    Add files via upload

---
+++ aula11/ex08.js
@@ -0,0 +1,5 @@
+var velocidade = 60.5
+console.log(`A velocidade do seu carro é ${velocidade} Km/h.`)
+if (velocidade > 60) { // Condição simples
+    console.log("Você ultrapassou a velocidade permitida na via. MULTADO!")
+}
